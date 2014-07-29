8c5850bfadb11d92761c446e4994bd5d36ed48de
spovalihin@gmail.com <spovalihin@gmail.com@74b3176d-45c4-4b80-9022-e50c74be2ad4>
2014-07-29 10:56:46 +0000

    let's try to delete the 'function-pointers' branch as it was based on my (formerly) imprecise understanding of x86 branch prediction, confused people, made the code uglier and didn't provide any measurable speedup

--- trunk/mylittle-fptr/externfunc.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __EXTERNFUNC_H__
-#define __EXTERNFUNC_H__
-
-extern STRICTINLINE UINT32 dz_compress(UINT32 value);
-extern STRICTINLINE INT32 CLIP(INT32 value,INT32 min,INT32 max);
-
-#endif
